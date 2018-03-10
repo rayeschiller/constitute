9cdb8a8de584316eaa063e09542e6863ee4e2868
Rina Schiller <schiller.rina@gmail.com>
2018-03-10 16:58:08 -0500

    database changes

--- pst/models.py
+++ pst/models.py
@@ -4,17 +4,17 @@ from django.db import models
 class Tweet(models.Model):
 	text = models.CharField(max_length=255)
 	username = models.CharField(max_length=255)
-	isRetweet = models.BooleanField(initial=False)
-	date = models.DateField()
+	# isRetweet = models.BooleanField(initial=False)
+	date = models.DateTimeField(auto_now=True)
 
 class Politican(models.Model):
 	first_name = models.CharField(max_length=255)
 	last_name = models.CharField(max_length=255)
-	username = mdoels.CharField(max_length=255)
+	username = models.CharField(max_length=255)
 
-class sexistWord(models.Model):
+class SexistWord(models.Model):
 	word = models.CharField(max_length=255)
-	
+
 
 
 
