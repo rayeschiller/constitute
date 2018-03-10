3638128811a378f05d08b27499560b127363a8c7
Rina Schiller <schiller.rina@gmail.com>
2018-03-10 16:30:15 -0500

    data model change

--- pst/models.py
+++ pst/models.py
@@ -2,6 +2,20 @@ from django.db import models
 
 # Create your models here.
 class Tweet(models.Model):
-	tweet_text = models.CharField(max_length=255)
+	text = models.CharField(max_length=255)
 	username = models.CharField(max_length=255)
+	isRetweet = models.BooleanField(initial=False)
+	date = models.DateField()
+
+class Politican(models.Model):
+	first_name = models.CharField(max_length=255)
+	last_name = models.CharField(max_length=255)
+	username = mdoels.CharField(max_length=255)
+
+class sexistWord(models.Model):
+	word = models.CharField(max_length=255)
+	
+
+
+
 	
