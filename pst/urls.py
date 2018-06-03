4c886d6419885c4d823d164e5e6b03b60d0cf385
natalie <natalie.doppstadt@gmail.com>
2018-06-03 18:16:59 -0400

    added react dependencies

--- pst/urls.py
+++ pst/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
-	path('', views.index, name='index'),
+	 path('admin/', admin.site.urls),
+  # path('api/', include('mynewapp.urls')),
+     path('', views.index, name='index'),
+  #re_path('index/', TemplateView.as_view(template_name='index.html')),
 
 ]
+# path('', views.index, name='index'),
