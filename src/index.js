0568cae9b0e2b8d84e6fccf433e7974c64ae762b
natalie <natalie.doppstadt@gmail.com>
2018-07-15 18:53:06 -0400

    created very basic front end component with embedded tweets

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App tweets="here"/>, document.getElementById('root'));
 registerServiceWorker();
