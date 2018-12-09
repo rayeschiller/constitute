3d0b92302c275eb4d4d9e56f09dc4ca25fee4f42
Rina Schiller <schiller.rina@gmail.com>
2018-12-09 15:33:19 -0500

    react heroku set up

--- siteadmin/settings.py
+++ siteadmin/settings.py
@@ -16,6 +16,9 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
