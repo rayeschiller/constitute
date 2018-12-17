5ecf7d5121ed71da2c5852398ad812a08f385a33
Juliana Emmanuelli <julianaemmanuelli@Julianas-MBP.home>
2018-12-16 19:16:02 -0500

    adding streamer

--- pst/streamListener.py
+++ pst/streamListener.py
@@ -3,9 +3,12 @@ from .tweetProcessing import processTweet
 
 class StreamListener(tweepy.StreamListener): 
     def on_status(self, status):
+        if status.retweeted_status:
+            return
         processTweet(status._json)
         print(status.text)
         print(status.created_at)
+
 
     def on_error(self, status_code):
         if status_code == 420:
