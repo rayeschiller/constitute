294f19dfed78c365f0e06203ec1253869beb0999
Rina Schiller <schiller.rina@gmail.com>
2018-12-16 22:46:02 -0500

    added to consumer

---
+++ pst/consumers.py
@@ -0,0 +1,8 @@
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+import json
+
+# def ws_connect(message):
+#     message.reply_channel.send({
+#         'accept': True
+#     })
