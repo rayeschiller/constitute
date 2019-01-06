de59cb644d2ae03bc1396ecf3482a353cb210adb
Rina Schiller <schiller.rina@gmail.com>
2019-01-05 21:43:33 -0500

    fixed celery import

--- siteadmin/celery.py
+++ siteadmin/celery.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 from pst.tasks import test
-from crontab import crontab
+from celery.schedules import crontab
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'siteadmin.settings')
 
@@ -20,6 +20,7 @@ app.autodiscover_tasks()
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     # Calls test('hello') every 10 seconds.
+    print('cleery started')
     sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
 
     # Calls test('world') every 30 seconds
