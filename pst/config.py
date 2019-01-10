0db78b279ac9d99b2fac5e9af4bea814e157f275
Rina Schiller <schiller.rina@gmail.com>
2019-01-09 21:09:52 -0500

    fixed sasd words

--- pst/config.py
+++ pst/config.py
@@ -3,6 +3,11 @@ CONFIG = {
     "CONSUMER_SECRET": 'wt1ciclku2cftRrv1WrNY3sidoSbRQ3xSP74fKO1dafT1pVHzn',
     "ACCESS_TOKEN": '15718225-77FWg39DfjuZIMRv4aqfuiEd3tM9TbmBHIFenF2tQ',
     "ACCESS_TOKEN_SECRET": 'qx9uoD5yzsUWeBgzVqIzChO7rruAvNjhomKmqua9nsfpl',
-    "SEXISTWORDS": ['whore', 'pussy','cunt', 'skank', 'bitch', 'slut', 'rape'],
-    "POLITICIANS": ['HillaryClinton', 'AOC', 'Alexandria Ocasio-Cortez', 'Elizabeth Warren', 'SenWarren', 'ElizabethWarren', 'NancyPelosi', 'Nancy Pelosi']
+    # "SEXISTWORDS": ['whore', 'pussy','cunt', 'skank', 'bitch', 'slut', 'rape'],
+    # "POLITICIANS": ['HillaryClinton', 'AOC', 'Alexandria Ocasio-Cortez', 'Elizabeth Warren', 'SenWarren', 'ElizabethWarren', 'NancyPelosi', 'Nancy Pelosi']
+    "SEXISTWORDS": ['slut', 'rape'],
+    "POLITICIANS": ['HillaryClinton', 'AOC', 'SenWarren', 'ElizabethWarren', 'NancyPelosi', 'Nancy Pelosi']
+
+
+
 }
