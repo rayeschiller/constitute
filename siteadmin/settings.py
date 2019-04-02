8179dcd4ce606ff8d8afefa188e40e46a77a18e9
Rina Schiller <schiller.rina@gmail.com>
2019-04-01 22:59:28 -0400

    moved whitenoise middleware to top and removed comments from datavizdetails

--- siteadmin/settings.py
+++ siteadmin/settings.py
@@ -52,13 +52,13 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 STATICFILES_DIRS = [
