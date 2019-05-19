f3b97d234717a1fc5d3afc700bc459cd22b829bf
natalie <natalie.doppstadt@gmail.com>
2019-05-19 18:34:43 -0400

    changed logo again

--- src/App.js
+++ src/App.js
@@ -54,13 +54,13 @@ class App extends Component {
           <div className="collapse navbar-collapse" id="navbarCollapse">
             <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-                <a className="nav-link" href="/tweets">Tweets <span className="sr-only">(current)</span></a>
+                <a className="nav-link" href="/appTweets">Tweets <span className="sr-only">(current)</span></a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="/vis">Analytics <span className="sr-only">(current)</span></a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/about">About</a>
+                <a className="nav-link" href="/cloud">Cloud <span className="sr-only">(current)</span></a>
               </li>
             </ul>
           </div>
