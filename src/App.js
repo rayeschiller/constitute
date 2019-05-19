22d2c9a6a02a37ea3da6b93172cd03b9f2a938b0
natalie <natalie.doppstadt@gmail.com>
2019-05-19 17:06:30 -0400

    added carousel

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import {Tweet} from 'react-twitter-widgets';
 import logoFinal from './components/images/logoFinal.png';
 import Tweets from './components/Tweets';
+import About from './components/About';
 import Home from './components/Home';
 import Analytics from './components/Analytics';
 import Map from './components/Map';
@@ -34,26 +35,26 @@ class App extends Component {
     return (
      <div className="container">
         <header>
-        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-          <a class="navbar-brand" href="/home">Constitute</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+          <a className="navbar-brand" href="/home">Constitute</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="/tweets">Tweets <span class="sr-only">(current)</span></a>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+                <a className="nav-link" href="/tweets">Tweets <span className="sr-only">(current)</span></a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/vis">Analytics <span class="sr-only">(current)</span></a>
+              <li className="nav-item">
+                <a className="nav-link" href="/vis">Analytics <span className="sr-only">(current)</span></a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/about">About</a>
+              <li className="nav-item">
+                <a className="nav-link" href="/about">About</a>
               </li>
             </ul>
-            <form class="form-inline mt-2 mt-md-0">
-              <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
-              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            <form className="form-inline mt-2 mt-md-0">
+              <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
           </div>
         </nav>
@@ -93,7 +94,8 @@ class App extends Component {
          </nav> */}
        <div className="App-intro">
          <Switch>
-           <Route path="/about" component={Home}/>
+           <Route path="/home" component={Home}/>
+           <Route path="/about" component={About}/>
            <Route path="/cloud" render={(props)=> <Cloud></Cloud>} />
            <Route path="/tweets" render={(props) => <Tweets {...props} tweets="home" />} />
            <Route path="/genderTweets" render={(props) => <Tweets {...props} tweets="home" />} />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,103 +1,76 @@
 import React, { Component } from 'react';
+import aboutImg from './images/about-img.jpg';
+import constitute from './images/constitute.png';
+import pstImg from './images/pst-home-image.jpeg';
+import $ from 'jquery';
 import Helmet from 'react-helmet';
-import './styles.css'
-import $ from 'jquery'
-import logoFinal from './images/logoFinal.png'
-import aboutImg from './images/about-img.jpg'
-import rina from './images/rina.jpg'
-import jules from './images/jules.jpg'
-import nat from './images/nat.jpg'
-import Chart from './Chart'
+import Carousel from 'react-bootstrap/Carousel';
+import Chart from './Chart';
+
+
+
 
 class Home extends Component {
-  
-  componentDidMount () {
-    $(document).ready(function(){
-      $('.header').height($(window).height());
-     })
-  }
+    componentDidMount () {
+        console.log('inside home component');
+        $(document).ready(function(){
+          $('.header').height($(window).height());
+         })
+      }
 
-  render (){
-  return(
-  <div>
-      <Helmet bodyAttributes={{style: 'background-color : #b3cccc'}}/>
-      <header className="header">
-        <div className="overlay"></div>
-      </header>
-      <div className="container">
-        <div className="description ">
-          <h1>Constitute
-          <p>    
-          A gender driven political analytics design to shed light on online harassment and inequalities women and LQBTQ folx encounter when running for office.
-          </p>   
-        </h1>  
-      </div>
-    </div>
-    <div className="about">
-    <h1>Who We Are</h1>
-    <div className="row">
-      <div className="col-lg-4 col-md-4 col-sm-12">
-        <img src={aboutImg} className="img-fluid"/>
-        <span className="text-justify">Represention is power</span>
-      </div>
-   <div className="col-lg-8 col-md-8 col-sm-12 desc">
-     
-    <h3>Constitute
-      <img src={logoFinal}/>
-    </h3>
-    <p>
 
-The goal of Constitute is to raise awareness of online rhetoric towards female politicians and how that feeds into public perception during election cycles and effects outcomes.
-We want to: Use sexist words along with most prominent female politicians of the 2020 election cycle to create mappings and visualizations of the the candidates receiving the most online negatively as well as the words being and imagery being used to describe them. Compare against their male counterparts, and raise public awareness. 
-    </p>
-   </div>
-  </div>
-  </div>
+    render (){
+        return(
 
-  <div>
+            <React.Fragment>
+
+<Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={constitute}
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      {/* <h3>A gender driven analytics tool</h3> */}
+      {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
   <Chart></Chart>
+    {/* <img
+      className="d-block w-100"
+      src={aboutImg}
+    //   "?text=Second slide&bg=282c34"
+      alt="Third slide"
+    /> */}
+
+    <Carousel.Caption>
+      {/* <h3>Second slide label</h3> */}
+      {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={pstImg}
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      {/* <h3>Third slide label</h3> */}
+      {/* <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p> */}
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+<div className="container-fluid">
+    <h2>HERE </h2>
 </div>
 
-<div className="team">
-    <h1 className="text-center">Our Team</h1>
-  <div className="row">
-   <div className="col-lg-4 col-md-4 col-sm-12 item">
-   <div className='circular'>
-    <img src={nat} className="img-fluid" alt="team"/>
-    </div>
-    <div className="des">
-      Natalie
-     </div>
-    <div className="text-muted">Natalie is a full stack software engineer at JP Morgan in Asset and Wealth Management. She graduated from Colby College in Maine with a major in mathematics. Her interests include politics, coding and mathematics.</div>
-   </div>
-   <div className="col-lg-4 col-md-4 col-sm-12 item">
-   <div className='circular'>
-    <img src={rina} className="img-fluid" alt="team"/>
-    </div>
-    <div className="des">
-       Rina
-     </div>
-    <div className="text-muted">Rina is a software engineer at JPMorgan Chase. She graduated from Hunter college in NYC with a major in digital media and minors in computer science and gender studies. She is very passionate about politics, gender equality, technology, and short hair.</div>
-   </div>
-   <div className="col-lg-4 col-md-4 col-sm-12 item">
-   <div className='circular'>
-    <img src={jules} className="img-fluid" alt="team"/>
-    </div>
-    <div className="des">
-      Jules
-     </div>
-    <div className="text-muted">Jules is a developer a JP Morgan. She graduated from Baruch College with a degree in Information Systems, English, NYC Studies, and International Relations. She is passionate about creating inclusive spaces to discuss gender inequities.</div>
-   </div>
-  </div>
-</div>
+</React.Fragment>
 
-<nav class="navbar fixed-bottom navbar-light bg-dark">
-  <a class="navbar-brand" href="#">Together we can affect change!</a>
-</nav>
-</div>
-    )
-  }
+        );
+    }
+
 }
-
-
 export default Home;
