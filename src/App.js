676f817f2e7c88184ecff3ca01dfba049f5f8356
natalie <natalie.doppstadt@gmail.com>
2019-05-19 18:05:42 -0400

    changed logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
 import {Tweet} from 'react-twitter-widgets';
-import logoFinal from './components/images/logo.png';
+import logoFinal from './components/images/constitute.png';
 import Tweets from './components/Tweets';
 import Home from './components/Home';
 import Analytics from './components/Analytics';
@@ -28,8 +28,13 @@ const TweetList = (props) => {
   ); 
 }
 
+// var imgStyle = {
+//   margin-top: 15px;
+// }
+
 
 class App extends Component {
+
 
   render () {
     return (
@@ -38,7 +43,7 @@ class App extends Component {
         <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
         <a className="navbar-brand pull-left" href="/home">
           <div>
-          <img src={logoFinal} width='115' margintop='-100' /></div></a>
+          <img src={logoFinal} className="logoStyle" width='115' margintop='-100'/></div></a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -58,10 +63,6 @@ class App extends Component {
                 <a className="nav-link" href="/about">About</a>
               </li>
             </ul>
-            <form className="form-inline mt-2 mt-md-0">
-              <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
           </div>
         </nav>
       </header>
